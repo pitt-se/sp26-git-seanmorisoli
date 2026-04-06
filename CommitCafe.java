4cf99434be199e51b56ac9a10af5d7edfb9cff57
seanmorisoli <smm399@pitt.edu>
2026-04-06 13:29:58 -0400

    Update CommitCafe.java

--- CommitCafe.java
+++ CommitCafe.java
@@ -7,7 +7,10 @@ public class CommitCafe {
 
     public static void brew(String drink) {
         // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
+        cups++;
+        System.out.println("Cups is now: " + cups);
         // STUDENT_TODO_2B: Add a second brew-related improvement
+        
     }
 
     public static void printSummary() {
